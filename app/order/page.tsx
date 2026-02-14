295d1ff16da1697146875e5b0904d47b0fd489a4
himmlegimmle46-png <himmegimmle46@gmail.com>
2026-02-14 11:52:37 -0800

    Fix Vercel prerender for order page

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import React, { useMemo, useState, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 import { GlowCard, GlowButton, PageTitle } from "../../components/ui";
